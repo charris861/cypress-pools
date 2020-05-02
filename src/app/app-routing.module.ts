ca22e8dcee64cd5219e98918ec771cff5b029d19
Chris Harris <harrischristopher97@gmail.com>
2020-05-02 14:34:53 -0400

    committing stuff

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { HomeComponent } from './home/home.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { PoolsGalleryComponent } from './pools-gallery/pools-gallery.component';
+import { ProductsComponent } from './products/products.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'contact', component: ContactUsComponent },
+  { path: 'products', component: ProductsComponent },
   { path: 'about', component: AboutUsComponent },
   { path: 'pools', component: PoolsGalleryComponent }
 ];
